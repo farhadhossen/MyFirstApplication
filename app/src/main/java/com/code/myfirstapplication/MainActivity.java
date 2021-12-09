1cefb553f22101494fb73f644369f0286f801101
farhadhossen <imfarhad01@gmail.com>
2021-12-09 14:29:10 +0600

    done

--- app/src/main/java/com/code/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/code/myfirstapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.code.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.idTextView);
+        button = findViewById(R.id.idButtom);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Welcome to Android Studio");
+            }
+        });
     }
 }
